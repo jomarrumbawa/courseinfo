7455d320d204afe8362c47071cc10ceb2c49e440
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-08-08 14:36:05 -0700

    Remove components and import Course

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,29 @@
-const Header = (props) => <h1>{props.course.name}</h1>;
-
-const Content = (props) => {
-  return (
-    <>
-      <Part
-        part={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        part={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        part={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
-    </>
-  );
-};
-
-const Part = (props) => (
-  <p>
-    {props.part} {props.exercises}
-  </p>
-);
-const Footer = (props) => (
-  <p>
-    Number of exercises{' '}
-    {props.course.parts[0].exercises +
-      props.course.parts[1].exercises +
-      props.course.parts[2].exercises}
-  </p>
-);
+import Course from './components/Course';
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Footer course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
