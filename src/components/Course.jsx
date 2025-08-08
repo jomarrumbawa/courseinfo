6509c381cc8f69f789fb0b435329b014d5fa838c
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-08-08 14:35:28 -0700

    Create Course component

---
+++ src/components/Course.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Footer course={course} />
+    </div>
+  );
+};
+
+export default Course;
